e9f1e1c047d527d889fb7dd73596bae1bff0f5e0
trynaeat <jamesparks10@gmail.com>
2021-12-30 13:08:17 -0500

    Adding backup script

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+####################################
+#
+# Backup script.
+#
+####################################
+
+# What to backup. 
+backup_files="./Zomboid"
+
+# Where to backup to.
+dest="./backups"
+
+# Create archive filename.
+date=$(date +"%Y-%m-%d")
+archive_file="Zomboid-$date.tgz"
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$archive_file"
+echo
+
+# Backup the files using tar.
+tar czf $dest/$archive_file $backup_files
+
+# Print end status message.
+echo
+echo "Backup finished"
+date
+
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest
